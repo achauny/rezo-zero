3fc01cd37df02c7eb0f613166c9ee3c08e187fad
achauny <achauny@blizz.fr>
2024-05-31 19:00:13 +0200

    :sparkles: feat(Event): Mise en place du GetCollection

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -2,25 +2,41 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\EventRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(normalizationContext: [
+            'groups' => ['event:read']
+        ]),
+    ],
+    order: ['name' => 'ASC'],
+    paginationEnabled: false
+)]
 class Event
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['event:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['event:read'])]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['event:read'])]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['event:read'])]
     private ?\DateTimeInterface $dateStart = null;
 
     public function getId(): ?int
